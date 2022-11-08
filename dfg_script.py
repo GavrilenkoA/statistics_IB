6f5932ca5671073d925e83881040fbf78691c259
GavrilenkoA <92908421+GavrilenkoA@users.noreply.github.com>
2022-11-09 02:49:07 +0700

    Add files via upload

---
+++ dfg_script.py
@@ -0,0 +1,8 @@
+import import_ipynb
+
+
+#%%
+import HW3
+#%%
+
+HW3.dfg_analyze(b_cell_path = "./b_cells_expression_data.csv", nk_cell_path = "./nk_cells_expression_data.csv", multiple_corrections= 1)
